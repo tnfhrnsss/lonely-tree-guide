c7098d418d357e0f74e28db7295052325e5875f9
tnfhrns <tnfhrns@gmail.com>
2025-08-03 00:15:34 +0900

    deploy path change dist -> docs

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  build: {
+    outDir: 'docs',
+  },
   plugins: [
     react(),
     mode === 'development' &&
