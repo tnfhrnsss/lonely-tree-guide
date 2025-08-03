7d5d00f9d2ed8b070a26928b10ad7354b69a89a2
tnfhrns <tnfhrns@gmail.com>
2025-08-03 12:17:53 +0900

    deploy path change dist -> docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/lonely-tree-guide/' : '/',
+  base: '/lonely-tree-guide/',
   server: {
     host: "::",
     port: 8080,
